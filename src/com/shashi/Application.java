671c45e3ddcda69dbdbea4931cedefebf6c54100
Shashi Bhushan <shashib@intelligrape.com>
2015-05-28 21:37:26 +0530

    1. Using Bean Factory

--- src/com/shashi/Application.java
+++ src/com/shashi/Application.java
@@ -1,7 +1,18 @@
 package com.shashi;
 
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.beans.factory.xml.XmlBeanFactory;
+import org.springframework.core.io.FileSystemResource;
+
+import com.shashi.shapes.Triangle;
+
 public class Application {
 	public static void main(String...strings){
 		
+		BeanFactory beanFactory = new XmlBeanFactory(new FileSystemResource("spring.xml"));
+		Triangle triangle = (Triangle)beanFactory.getBean("triangle");
+		
+		System.out.println("Initializing from Bean factory : ");
+		triangle.drawShape();
 	}
 }
--- src/com/shashi/shapes/Triangle.java
+++ src/com/shashi/shapes/Triangle.java
@@ -2,5 +2,50 @@ package com.shashi.shapes;
 
 public class Triangle {
 
+	private String type;
+	private int side;
 	
+	/*
+	 * Constructors 
+	 */
+	public Triangle(String type) {
+		this.type = type;
+	}
+
+	public Triangle(int side) {
+		super();
+		this.side = side;
+	}
+
+	public Triangle(String type, int side) {
+		super();
+		this.type = type;
+		this.side = side;
+	}
+
+	/*
+	 * Getters and Setters
+	 */
+	public void setType(String type){
+		this.type = type;
+	}
+	
+	public String getType(){
+		return type;
+	}
+	
+	public int getSide() {
+		return side;
+	}
+
+	public void setSide(int side) {
+		this.side = side;
+	}
+
+	/*
+	 * Draw shape Method
+	 */
+	public void drawShape(){
+		System.out.println( "drawing Traingle Shape of Type :" + this.getType() + " and Side : " + this.getSide() );
+	}
 }

c6d09f9e717d7fcd1d7a3c79880af20a701adc33
Shashi Bhushan <shashib@intelligrape.com>
2015-05-29 20:56:18 +0530

    10. Singleton Bean Scope

--- src/com/shashi/Application.java
+++ src/com/shashi/Application.java
@@ -4,6 +4,7 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 import com.shashi.shapes.Triangle;
+import com.shashi.shapes.point.Point;
 
 public class Application {
 	public static void main(String...strings){
@@ -14,5 +15,19 @@ public class Application {
 		System.out.println("Initializing from Application Context : ");
 		triangle.drawShape();
 		
+		Point point = new Point();
+		point.setX(35);
+		point.setY(70);
+		triangle.setPointA(point);
+		triangle.drawShape();
+
+		/*
+		 * This bean is the same as previous bean
+		 * Single object
+		 */
+		Triangle secondTriangle = (Triangle)context.getBean("triangle-alias");
+		
+		System.out.println("Initializing from Application Context : ");
+		secondTriangle.drawShape();
 	}
 }

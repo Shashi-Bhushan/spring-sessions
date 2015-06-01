b1d4bea4e7cbcedd96078bd50719e21d6eb2a1d2
Shashi Bhushan <shashib@intelligrape.com>
2015-06-01 10:34:40 +0530

    12. ApplicationContextAware and BeanNameAwre

--- src/com/shashi/Application.java
+++ src/com/shashi/Application.java
@@ -9,6 +9,14 @@ import com.shashi.shapes.point.Point;
 public class Application {
 	public static void main(String...strings){
 		
+		/*
+		 * if you want to have this context available anywhere else
+		 * For Eg. in case when points of different traingles needs to be different.
+		 * [
+		 * 	in previous commits, Traingle was a Singleton so it's Points will be Singleton as well.
+		 * 	Result: different Traingle references but having same points
+		 * ]
+		 */
 		ApplicationContext context = new ClassPathXmlApplicationContext("spring.xml");
 		Triangle triangle = (Triangle)context.getBean("triangle-alias");
 		
--- src/com/shashi/shapes/Triangle.java
+++ src/com/shashi/shapes/Triangle.java
@@ -1,12 +1,22 @@
 package com.shashi.shapes;
 
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.BeanNameAware;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
+
 import com.shashi.shapes.point.Point;
 
-public class Triangle {
+/*
+ * ApplicationContextAware : to get hold of context object initialized in Application class
+ * BeanNameAware : to use the bean Name. I'm just printing the bean name representing this(TRIANGLE) class in xml file 
+ */
+public class Triangle implements ApplicationContextAware,BeanNameAware {
 
 	private Point pointA;
 	private Point pointB;
 	private Point pointC;
+	private ApplicationContext context;
 	
 	public Point getPointA(){
 		return pointA;
@@ -34,5 +44,19 @@ public class Triangle {
 	
 	public void drawShape(){
 		System.out.println( "Point A : " + this.getPointA() + " | Point B : " + this.getPointB() + " | Point C : " + this.getPointC() );
+	}
+
+	@Override
+	public void setApplicationContext(ApplicationContext context)
+			throws BeansException {
+		// TODO Auto-generated method stub
+		System.out.println("ApplicationContext is : " + context);
+		this.context = context;
+	}
+
+	@Override
+	public void setBeanName(String beanName) {
+		// TODO Auto-generated method stub
+		System.out.println("Bean Name is : " + beanName);
 	}
 }

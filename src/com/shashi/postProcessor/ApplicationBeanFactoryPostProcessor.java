673f7004cfb0f8cc6388fbf8c05687719aec3ea3
Shashi Bhushan <shashib@intelligrape.com>
2015-06-03 17:45:59 +0530

    20. PropertyPlaceholderConfigurer - Configure Property from XML File

--- src/com/shashi/postProcessor/ApplicationBeanFactoryPostProcessor.java
+++ src/com/shashi/postProcessor/ApplicationBeanFactoryPostProcessor.java
@@ -4,6 +4,14 @@ import org.springframework.beans.BeansException;
 import org.springframework.beans.factory.config.BeanFactoryPostProcessor;
 import org.springframework.beans.factory.config.ConfigurableListableBeanFactory;
 
+/*
+ * Overriding default behavior when BeanFactory is initialized
+ * called whenever BeanFactory(or ApplicationContext) is initialized
+ * 
+ * PropertyPlaceholderConfigurer : OOTB BeanFactoryPostProcessor that Spring Provides
+ * for Eg. i don't want to add the Point values "0,0" in spring.xml 
+ * i want to add the values somewhere else in an xml file and then load it in spring.xml
+ */
 public class ApplicationBeanFactoryPostProcessor implements BeanFactoryPostProcessor {
 
 	@Override
--- src/com/shashi/shapes/point/Point.java
+++ src/com/shashi/shapes/point/Point.java
@@ -25,3 +25,4 @@ public class Point {
 		return this.getX() + " - " + this.getY();
 	}
 }
+

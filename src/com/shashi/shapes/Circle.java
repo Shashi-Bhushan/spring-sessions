7696681dabe47ada3cda4c710bb248850581cfd6
Shashi Bhushan <shashib@intelligrape.com>
2015-06-04 10:37:00 +0530

    25. Resource(JSR's Autowire) - PostConstruct - PreDestroy Annotations

--- src/com/shashi/shapes/Circle.java
+++ src/com/shashi/shapes/Circle.java
@@ -1,8 +1,8 @@
 package com.shashi.shapes;
 
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+import javax.annotation.Resource;
 
 import com.shashi.Shapes;
 import com.shashi.shapes.point.Point;
@@ -12,13 +12,13 @@ public class Circle implements Shapes {
 	private Point center;
 	
 	/*
-	 * Marks that this setter is to be Autowired by Type
+	 * Marks that this setter is a Resource 
 	 * 
-	 * AutowiredAnnotationBeanPostProcessor bean needs to be created as well for this.
-	 * their is an unambiguous byType Point Bean ins pring.xml so it'll resolve and autowire that Bean in this Setter function 
+	 * Resource is a JSR-250 annotation(from javax.annotation package) i.e. it's not specific to Spring.
+	 * it's capable of injecting byName but if i won't specify the name here and their is a bean named center in spring.xml
+	 * then that ean will be injected in this setter.
 	 */
-	@Autowired
-	@Qualifier("circleRelated")
+	@Resource(name="pointC")
 	public void setCenter(Point center){
 		this.center = center;
 	}
@@ -30,6 +30,21 @@ public class Circle implements Shapes {
 	@Override
 	public void drawShape() {
 		System.out.println("Center Point of circle is : ( " + this.center.getX() + ", " + this.center.getY() + " )");
+	}
+	
+	@PostConstruct
+	public void initializeCircle(){
+		System.out.println("Init Method of Circle.");
+	}
+	
+	/*
+	 * only work if the shutdown hook is registered.
+	 * for that need to call
+	 * 		-> abstractApplicationContextObject.registerShutdownHook();
+	 */
+	@PreDestroy
+	public void disposeCircle(){
+		System.out.println("Dispose Method of Circle");
 	}
 
 }
